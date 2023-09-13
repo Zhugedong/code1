6962baf92caa04745e4f2c19b8c12b3ce30cf2c2
Zhugedong <2360421118@qq.com>
2023-09-13 17:36:15 +0800

    Frist commit

---
+++ build/CMakeFiles/efficientnet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/efficientnet.dir/efficientnet.cpp.o"
+  "CMakeFiles/efficientnet.dir/efficientnet.cpp.o.d"
+  "efficientnet"
+  "efficientnet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/efficientnet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/efficientnet.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for efficientnet.
+# This may be replaced when dependencies are built.
